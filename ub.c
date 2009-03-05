02f917c00ce462a0361679de39fa9eab82e8e444
Sven Ulland <svensven@gmail.com>
2009-03-05 22:00:44 +0100

    Reads and resolves list of domains from stdin.

--- ub.c
+++ ub.c
@@ -5,10 +5,11 @@
 #include <unbound.h>
 
 #define QNAME_MAX 255
+#define QUEUE_MAX 10
 
 /* This is called when resolution is completed */
 void mycallback(void* mydata, int err, struct ub_result* result)
-{	
+{
 	int* qlen = (int*)mydata;
 	(*qlen)--;
 	if(err != 0) {
@@ -28,9 +29,14 @@ int main(void)
 	struct ub_ctx* ctx;
 	volatile int qlen = 0;
 	int retval;
-	int i = 0;
 	char qname[QNAME_MAX];
-	char *nl;
+	char *moredata, *nl;
+
+	/* basic checks */
+	if(QUEUE_MAX < 1) {
+		printf("error: queue length must be >0\n");
+		return 1;
+	}
 
 	/* create context */
 	ctx = ub_ctx_create();
@@ -40,31 +46,34 @@ int main(void)
 	}
 
 	/* we keep running, lets do something while waiting */
+	moredata = (char*)-1;
 	do {
-		/* read and prepare qname */
-		if(fgets(qname, QNAME_MAX, stdin) != NULL) {
-			nl = strrchr(qname, '\n');
-			if(nl) *nl = '\0';
-			printf("debug: ok, just read \"%s\" from stdin\n", qname);
+		/* queue has room && more data on stdin? */
+		if(qlen < QUEUE_MAX && moredata) {
+			/* read and prepare qname from stdin */
+			moredata = fgets(qname, QNAME_MAX, stdin);
+			if(moredata != NULL) {
+				nl = strrchr(qname, '\n');
+				if(nl) *nl = '\0';
 
-			/* add async query to queue */
-			retval = ub_resolve_async(ctx, qname, 1, 1, (void*)&qlen, mycallback, NULL);
+				/* add async query to queue */
+				retval = ub_resolve_async(ctx, qname, 1, 1, (void*)&qlen, mycallback, NULL);
+				if(retval != 0) {
+					printf("resolve error for %s: %s\n", qname, ub_strerror(retval));
+					continue;
+				}
+				qlen++;
+			}
+		} else {
+			/* queue is full || eof stdin reached */
+			usleep(100000); /* wait 1/10 of a second */
+			retval = ub_process(ctx);
 			if(retval != 0) {
 				printf("resolve error: %s\n", ub_strerror(retval));
 				return 1;
 			}
-			qlen++;
 		}
-
-		usleep(100000); /* wait 1/10 of a second */
-		printf("time passed (%d) .. qlen is %d\n", i++, qlen);
-		retval = ub_process(ctx);
-		if(retval != 0) {
-			printf("resolve error: %s\n", ub_strerror(retval));
-			return 1;
-		}
-	} while(qlen);
-	printf("done\n");
+	} while(qlen || moredata);
 
 	ub_ctx_delete(ctx);
 	return 0;
